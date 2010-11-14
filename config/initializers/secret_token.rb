b2d5da9cc29f3fe2a40987e0e49cbe3e7c9d0fdb
Brandur <fyrerise@mutelight.org>
2010-11-13 22:37:01 -0700

    * initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Askja::Application.config.secret_token = '86bdab2c17baef898593457b9187937fdb7f96a2ab984d0620525173d4fe556b60ea4d87b3da905ad0a59a8e1c5532f455dfb7d90e05da6f5fcc97d1302337b9'
