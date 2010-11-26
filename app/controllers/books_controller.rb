4b3511010b4885beaf23824345db5665cf58d664
Brandur <fyrerise@mutelight.org>
2010-11-25 23:10:24 -0700

    * major improvements to books across the board

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @order = Book.where('finished_at < ?', @book.finished_at).where('finished_at > ?', @book.finished_at.year).count + 1
 
     respond_to do |format|
       format.html
