91d7b1a73e1e791a5f5ee88e44900bf8b9231786
Brandur <fyrerise@mutelight.org>
2011-03-11 07:19:56 -0700

    * less frequent updates due to EC2 instances being generally slow

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-every 5.minutes do
+every 30.minutes do
   rake 'update'
 end
 
